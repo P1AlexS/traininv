7d1df3b6fb70a99b6840305fc9d31a5fe69bafa7
Alex <alexander.suchland@studmail.w-hs.de>
2024-08-06 18:04:10 +0200

    added gradle prototyp app

---
+++ app.py
@@ -0,0 +1,40 @@
+import gradio as gr
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+import os
+from PIL import Image
+
+directory = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models'))
+model_path = os.path.join(directory, 'model.keras')
+model = tf.keras.models.load_model(model_path)
+
+
+def predict_image(img):
+    if isinstance(img, np.ndarray):
+        img = Image.fromarray(img)
+
+    image_size = 100
+    img = img.resize((image_size, image_size))
+
+    img_arr = np.array(img)
+    img_arr = np.expand_dims(img_arr, axis=0)
+    img_arr = img_arr / 255.0
+
+    predictions = model.predict(img_arr)
+    predicted_class = np.argmax(predictions, axis=1)[0]
+    class_names = ['cat', 'dog']
+
+    return {class_names[predicted_class]: float(predictions[0][predicted_class])}
+
+demo = gr.Interface(
+    fn=predict_image,
+    inputs=gr.Image(),
+    outputs=gr.Label(num_top_classes=2),
+    title="Bildklassifikator",
+    description="Laden Sie ein Bild hoch, um eine Klassifikation durchzuführen."
+)
+
+# Gradio-Anwendung starten
+demo.launch()
